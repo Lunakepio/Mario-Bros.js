98b9eef0cb82178d19b2e5b88cd95a9f5588aad3
Lunakepio <moulinneufalex@gmail.com>
2024-04-19 14:56:59 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { OrbitControls } from "@react-three/drei";
+import { KeyboardControls, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React, { useRef, useEffect, Suspense } from "react";
+import React, { useRef, useEffect, Suspense, useMemo } from "react";
 import { Experience } from "./Experience";
 
+export const Controls = {
+  left: "left",
+  right: "right",
+  jump: "jump",
+};
+
 function App() {
+  const map = useMemo(
+    () => [
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
+    ],
+    []
+  );
+
   return (
     <>
       <div className="canvas-container">
-        <Canvas>
-          <Suspense fallback={null}>
-            <Experience />
-          </Suspense>
-        </Canvas>
+        <KeyboardControls map={map}>
+          <Canvas>
+            <Suspense fallback={null}>
+              <Experience />
+            </Suspense>
+          </Canvas>
+        </KeyboardControls>
       </div>
     </>
   );
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,33 +1,65 @@
-import React, { useRef, useEffect, Suspense } from "react";
-import { OrbitControls } from "@react-three/drei";
+import React, { useRef, useEffect, Suspense, useState } from "react";
+import {
+  Environment,
+  OrbitControls,
+  OrthographicCamera,
+  PerspectiveCamera,
+  useKeyboardControls,
+} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-
+import { Model } from "./Mario";
+import { Level } from "./Mario_level";
+import { Controls } from "./App";
 export const Experience = () => {
   const meshRef = useRef();
   const PI = Math.PI;
+  const camera = useRef();
+  const groupRef = useRef();
+  const [, get] = useKeyboardControls();
+  const [animation, setAnimation] = useState("mixamo.com");
   
-  useGSAP(() => {
-    gsap.to(meshRef.current.rotation, {
-      y: PI * 2,
-      duration: 3,
-      yoyo: -1,
-      repeat: -1,
-      ease: "expo.inOut"
-    })
-  },[])
+
+  useFrame(() => {
+    const character = groupRef.current;
+    const goLeft = get()[Controls.left];
+    const goRight = get()[Controls.right];
+    const jump = get()[Controls.jump];
+    setAnimation("mixamo.com");
+    if(goLeft){
+      setAnimation("Run");
+    }
+    
+    if(goRight){
+      
+    }
+    if(jump){
+      
+    }
+  });
+
   return (
     <>
-      <mesh ref={meshRef}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"#FF00FF"} />
-        <axesHelper />
-      </mesh>
+      <group ref={groupRef} position={[0, 2, 0]}>
+        <Model animation={animation}/>
+        <PerspectiveCamera
+          position={[-10, 2, 0]}
+          near={0.1}
+          far={1000}
+          fov={35}
+          rotation={[0, -PI / 2, 0]}
+          makeDefault
+        />
+      </group>
+      <Level />
       <ambientLight intensity={1} />
-      <pointLight position={[2, 2, 2]} intensity={50}>
-        <axesHelper />
-      </pointLight>
-      <OrbitControls />
+      <pointLight position={[2, 2, 2]} intensity={50}></pointLight>
+      {/* <Model/> */}
+      {/* <OrbitControls /> */}
+      
+
+      <Environment preset="warehouse" background />
     </>
   );
 };
